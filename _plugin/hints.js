e68dc606c375f4af614dd993580a77b0c169184c
bon <bon@bon-v.tube>
2026-06-15 17:20:22 -0400

    intro letter. moving hints to hint directory, renaming them

--- _plugin/hints.js
+++ _plugin/hints.js
@@ -1,35 +1,49 @@
 export default [
     {
-        "path": "0bdd6523-9987-49eb-9db9-32b90d06ad91.png",
+        "path": "letter.html",
         "modified": "01-Jan-1996 00:00:00",
-        "size": "33 k",
+        "desc": "Letter to Shinri"
+    },
+    {
+        "path": "0bdd6523-9987-49eb-9db9-32b90d06ad91.jpg",
+        "modified": "01-Jan-1996 00:00:00",
+        "size": "19 k",
         "desc": "hint 01"
     },
-    // {
-    //     "path": "/category/cryptid-sightings/question/thread/#post-10",
-    //     "modified": "01-Jan-1996 00:00:00",
-    //     "desc": "hint 02"
-    // },
-    // {
-    //     "path": "/category/general-discussion/spooky-stories/thread/#post-12",
-    //     "modified": "01-Jan-1996 00:00:00",
-    //     "desc": "hint 03"
-    // },
     {
-        "path": "9b04d2c2-270f-4ef1-8b50-c29b9df05775.png",
+        "path": "39192035-c10e-411f-94ab-f06623dddb03.jpg",
         "modified": "01-Jan-1996 00:00:00",
-        "size": "12 k",
+        "size": "8 k",
+        "desc": "hint 02"
+    },
+    {
+        "path": "117b23fa-c3c6-4ff6-a87c-6c2f4a99135d.jpg",
+        "modified": "01-Jan-1996 00:00:00",
+        "size": "8 k",
+        "desc": "hint 03"
+    },
+    {
+        "path": "9b04d2c2-270f-4ef1-8b50-c29b9df05775.jpg",
+        "modified": "01-Jan-1996 00:00:00",
+        "size": "15 k",
         "desc": "hint 04"
     },
     {
-        "path": "9b04d2c2-270f-4ef1-8b50-c29b9df05775.png",
+        "path": "32559787-1c2f-46e2-9e86-7651c976e5a1.jpg",
         "modified": "01-Jan-1996 00:00:00",
         "size": "12 k",
         "desc": "hint 05"
     },
-    // {
-    //     "path": "#",
-    //     "modified": "01-Jan-1996 00:00:00",
-    //     "desc": "hint 06"
-    // },
+    {
+        "path": "4b641ad6-d74a-4c21-ac63-af086080da04.jpg",
+        "modified": "01-Jan-1996 00:00:00",
+        "size": "16 k",
+        "desc": "hint 06"
+    },
+    {
+        "path": "cb628284-3ed3-4a6c-b62b-75f2136d166b.jpg",
+        "modified": "01-Jan-1996 00:00:00",
+        "size": "16 k",
+        "desc": "hint 07"
+    },
 ]
